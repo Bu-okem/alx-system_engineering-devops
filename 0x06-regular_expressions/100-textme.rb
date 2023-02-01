c94ec18819bfbaae98f85eb9d7fb99948f892479
Chibuokem Obiegbulem <chiobiegbulem@gmail.com>
2023-01-31 20:56:04 -0800

    Task 100

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,3 @@
 #!/usr/bin/env ruby
-res = ARGV[0].scan(/\[from:(.+?)\]|\[to:(.+?)\]|\[flags:(.+?)\]/)
-list = [res[0].compact, res[1].compact, res[2].compact]
-puts list.join(',')
+puts ARGV[0].scan(/\[from:(.\w*)\]\s\[to:(\W*\d*)\]\s\[flags:((?:-?\d:?)+)/)
+  .join(separator=',')
