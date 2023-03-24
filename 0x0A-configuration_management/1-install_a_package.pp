e1943034e6d6c1fb0fcf0e86912c934a4603c9b5
Chibuokem Obiegbulem <chiobiegbulem@gmail.com>
2023-03-24 22:57:43 +0100

    Task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,11 @@
 # install flask from pip3
-package { 'flask':
-    ensure  => '2.1.0',
+package { 'python3-pip':
+  ensure => installed,
 }
+
+exec { 'install-flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => '/usr/bin:/usr/sbin:/bin',
+  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+}
+
